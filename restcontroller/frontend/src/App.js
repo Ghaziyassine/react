e1dcbf18ea2428925a338ed70c394e6babd6d61f
ihssanf <ihssane.elmaizi02@gmail.com>
2024-11-20 18:14:34 +0100

    first commit

---
+++ restcontroller/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CompteManager from "./component/CompteManager";
+
+const App = () => {
+  return (
+    <div>
+      <CompteManager />
+    </div>
+  );
+};
+
+export default App;
